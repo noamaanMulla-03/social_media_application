9773ee4147a8bd078636e1b021f22f081c13d9f9
Noamaan Mulla <noamaan.mulla03@gmail.com>
2022-07-02 22:19:00 +0530

    Created mongodb atlas server and connected it

--- server/index.js
+++ server/index.js
@@ -2,3 +2,20 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+
+app.use(bodyParser.json({ limit: '50mb', extended: true }));
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
+app.use(cors());
+
+const MONGO_CONNECTION_URL = `mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@cluster0.jq6q8.mongodb.net/?retryWrites=true&w=majority`;
+
+const CONNECTION_PORT = process.env.PORT || 5000;
+
+mongoose.connect(MONGO_CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => app.listen(CONNECTION_PORT, () => console.log(`\n[-] Server running on port ${CONNECTION_PORT}...\n`)))
+    .catch(err => console.log(`\n[-] Error connecting to database: ${err}\n`));
