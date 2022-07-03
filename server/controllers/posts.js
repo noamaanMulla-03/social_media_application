ddf06f6da18f62b2eac57dfd62a7728abd614908
Noamaan Mulla <noamaan.mulla03@gmail.com>
2022-07-03 14:06:55 +0530

    created models for post messages and controller logic for creating and getting posts

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,5 +1,25 @@
-const getPosts = (req, res) => {
-    res.send("Hello World");
+import PostMessagesModel from "../models/postMessages.js";
+
+const getPosts = async (req, res) => {
+    try {
+        const postMessages = await PostMessagesModel.find();
+        res.status(200).json(postMessages);
+
+    } catch (error) {
+        res.status(404).json({ message: error.message });
+    }
 };
 
-export { getPosts };
+const createPosts = async (req, res) => {
+    const POST = req.body;
+    const newPostMessage = new PostMessagesModel(POST);
+
+    try {
+        await newPostMessage.save();
+        res.status(201).json(newPostMessage);
+    } catch (error) {
+        res.status(409).json({ message: error.message });
+    }
+};
+
+export { getPosts, createPosts };
---
+++ server/models/postMessages.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+
+const postMessageSchema = mongoose.Schema({
+    title: String,
+    message: String,
+    creator: String,
+    tags: [String],
+    selectedFile: String,
+    likeCount: {
+        type: Number,
+        default: 0,
+    },
+    createdAt: {
+        type: Date,
+        default: new Date(),
+    },
+});
+
+const PostMessagesModel = mongoose.model("PostMessagesModel", postMessageSchema);
+
+export default PostMessagesModel;
